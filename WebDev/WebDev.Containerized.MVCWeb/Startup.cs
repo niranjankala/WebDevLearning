a73efa7c94fafda6dfa92926e97fda33dd4d3604
Niranjan Singh <niranjan.singh@mind-infotech.com>
2019-01-23 12:57:32 +0000

    added docker compose files but sql server image path was not working

--- WebDev/WebDev.Containerized.MVCWeb/Startup.cs
+++ WebDev/WebDev.Containerized.MVCWeb/Startup.cs
@@ -32,7 +32,7 @@ namespace WebDev.Containerized.MVCWeb
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
